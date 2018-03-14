85516bb047ca2fe6199ea6957a995d3b4423c69f
root <root@Luis.localdomain>
2018-03-14 12:49:37 +0100

    Primeros pasos

---
+++ securebox_client.py
@@ -0,0 +1,7 @@
+import python *
+
+
+
+applicant = input(">>>")
+time = input("Enter the appointment time: ")
+print(interviewer, "will interview", applicant, "at", time)
---
+++ securebox_crypto.py
@@ -0,0 +1,33 @@
+import python *
+import pycryptodome *
+
+
+# utiliza HASH y devuelve clave  
+def anadir_firma(mensaje, ID_sender)
+
+# RSA con clave privada del sender
+def encriptar_firma(mensaje, ID_sender)
+
+# RSA con clave publica del sender
+def desencriptar_firma(firma, ID_sender)
+
+# cifra el mensaje+firma de manera simetrica usando AES
+def encriptar_AES()
+
+# descifra AES
+def desencriptar_AES(AES_key)
+
+def comprobar_hash(firma, hash)
+
+
+# coge la clave del AES del anadir firma y la cifra con RSA con la clave publica del receptor
+def anadir_sobre(key)
+
+# descifra la clave del AES con la clave privada del receptor
+def abrir_sobre(cKey)
+
+
+def encriptar_all()
+
+def desencriptar_all()
+

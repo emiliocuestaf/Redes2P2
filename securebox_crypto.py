6d8147d38b238b4739187984a6b527bf91acd434
root <root@Luis.localdomain>
2018-03-15 02:13:52 +0100

    Desencriptado

--- securebox_crypto.py
+++ securebox_crypto.py
@@ -1,5 +1,4 @@
 import python *
-import pycryptodome *
 import requests
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
@@ -8,6 +7,8 @@ from Crypto.Cipher import AES, PKCS1_OAEP
 
 IVLEN = 16
 AESCLEN = 32
+
+# Funciones para encriptar
 
 # utiliza HASH y crea la firma 
 
@@ -28,17 +29,17 @@ def encriptar_AES(mensaje, clave):
 	iv = get_random_bytes(IVLEN)
 
 	cifrado_aes = AES.new(clave, AES.MODE_CBC, iv)
-	return cipher_aes.encrypt(mensaje)
+	return iv + cipher_aes.encrypt(mensaje)
 
 
 # coge la clave del AES del anadir firma y la cifra con RSA con la clave publica del receptor
 
-def crear_sobre(clave, ID_usuario)
+def crear_sobre(clave, ID_receptor):
 	
 	# Creamos la solicitud de la clave publica al servidor 
 
 	url = 'https://vega.ii.uam.es:8080/api/users/getPublicKey'
-	args = {'userID': ID_usuario}
+	args = {'userID': ID_receptor}
 	r = requests.post(url, json=args)
 	
 	# r.text contiene la respuesta del servidor (la clave publica)
@@ -51,32 +52,9 @@ def crear_sobre(clave, ID_usuario)
 	return cifrador.encrypt(clave)
 
 
-
-# Encrypt the session key with the public RSA key
-
-# RSA con clave privada del sender
-#def encriptar_firma(mensaje, ID_sender)
-
-# RSA con clave publica del sender
-def desencriptar_firma(firma, ID_sender)
-
-
-
-
-
-# descifra AES
-def desencriptar_AES(AES_key)
-
-def comprobar_hash(firma, hash)
-
-# descifra la clave del AES con la clave privada del receptor
-def abrir_sobre(c_Key)
-
-
-
 # Se encarga de todo el proceso de cifrar un mensaje
 
-def encriptar_all(mensaje, ID_usuario):
+def encriptar_all(mensaje, ID_receptor):
 
 	# Firmamos el mensaje
 
@@ -89,11 +67,86 @@ def encriptar_all(mensaje, ID_usuario):
 
 	# Ciframos la clave para crear el sobre
 
-	sobre = crear_sobre(clave, ID_usuario)
+	sobre = crear_sobre(clave, ID_receptor)
 
 	return sobre + c_mensaje
 
+
+# Funciones para desencriptar
+
+# descifra la clave del AES con la clave privada del receptor
+def abrir_sobre(c_clave):
+
+	# Conseguimos la clave privada del usuario
+
+	clave_privada = RSA.import_key(open("noEsLaClave.dat", "r").read())
+
+	cifrador = PKCS1_OAEP.new(clave_privada)
+
+	return cifrador.decrypt(c_clave)
+
+
+# descifra AES
+def desencriptar_AES(clave, iv, c_mensaje):
+
+	cifrador = AES.new(clave, AES.MODE_CBC, iv)
+
+	return cifrador.decrypt(c_mensaje)
+
+# Devuelve el hash, gracias a RSA con la clave publica del emisor
+
+def desencriptar_firma(firma, ID_emisor):
+
+	# Creamos la solicitud de la clave publica al servidor 
+
+	url = 'https://vega.ii.uam.es:8080/api/users/getPublicKey'
+	args = {'userID': ID_emisor}
+	r = requests.post(url, json=args)
 	
+	# r.text contiene la respuesta del servidor (la clave publica)
 
-def desencriptar_all()
+	clave_publica = RSA.import_key(r.text)
 
+	cifrador = PKCS1_OAEP.new(clave_publica)
+
+	return cifrador.decrypt(firma)
+
+# Comprueba si la firma es valida o no
+
+def firma_valida(firma_descifrada, mensaje):
+
+	# Creamos el hash
+	h = SHA256.new()
+	# Lo aplicamos en el mensaje
+	h.update(mensaje)
+
+	if h.digest() == firma_descifrada:
+		return true
+	else:
+		return false
+
+
+# Funcion que se encarga de todo el proceso de desencriptacion
+
+def desencriptar_all(mensaje, ID_emisor):
+
+	# Separamos el mensaje cifrado por partes (primero va el sobre, luego iv y luego la firma y el mensaje cifrados)
+	sobre = mensaje[0:AESCLEN]
+
+	aux = AESCLEN+16
+	iv = mensaje[AESCLEN:aux]
+
+	aux2 = 100
+	firma = mensaje[aux: aux2] # CUANTO OCUPA LA FIRMA????
+
+	c_mensaje = mensaje[aux2:]
+
+	clave = abrir_sobre(sobre)
+	d_mensaje = desencriptar_AES(clave, iv, c_mensaje)
+	d_firma = desencriptar_firma(firma, ID_emisor)
+
+	if firma_valida(d_firma, d_mensaje):
+		return d_mensaje
+	else:
+		print "La firma no es válida."
+		return

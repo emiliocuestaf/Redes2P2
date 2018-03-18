0f6a2e9943dd4fe4bb7e0e72e75e07e675129c3a
emiliocuestaf <emiliocuestaf@gmail.com>
2018-03-18 23:32:17 +0100

    user listo2

--- securebox_client.py
+++ securebox_client.py
@@ -1,125 +1,140 @@
 import argparse
 import sys
-import securebox_files
-import securebox_crypto
-import securebox_users
+import ast
+import securebox_files as files 
+import securebox_crypto as crypto
+import securebox_users as users
 
+#Codigo sacado online
+def read_dictionary():
+	d = {}
+	with open("authorization.dat") as f:
+	    for line in f:
+	       (key, val) = line.split()
+	       d[key] = val
+	return d
 
 def main():
+
 	parser = argparse.ArgumentParser(description='Servidor de transferencia y codificacion de archivos interactivo.')
 
 	# Gestion de usuarios e identidades
-	#TODO: WORK WITHOUT ALIASE
-	parser.add_argument('--create_id', nargs=3, help='Registro de un usuario')
+	#TODO: WORK WITHOUT ALIAS
+	parser.add_argument('--create_id', nargs=3, help='Registro de un usuario', metavar=('nombre', 'email', 'alias'))
 
-	parser.add_argument('--search_id', nargs=1, help='Busqueda de un usuario')
+	parser.add_argument('--search_id', nargs=1, help='Busqueda de un usuario', metavar=('cadena'))
 
-	parser.add_argument('--delete_id', nargs=1,  help='Eliminacion de una identidad registrada')
+	parser.add_argument('--delete_id', nargs=1,  help='Eliminacion de una identidad registrada', metavar=('id'))
 
 	# Subida y descarga de ficheros
-	parser.add_argument('--upload', nargs=1, help='Subida de un fichero al servidor')
+	parser.add_argument('--upload', nargs=1, help='Subida de un fichero al servidor', metavar=('fichero'))
 
-	parser.add_argument('--source_id', nargs=1,  help='ID del emisor del fichero')
+	parser.add_argument('--source_id', nargs=1,  help='ID del emisor del fichero', metavar=('id'))
 
-	parser.add_argument('--dest_id', nargs=1, help='ID del receptor del fichero')
+	parser.add_argument('--dest_id', nargs=1, help='ID del receptor del fichero', metavar=('id'))
 
 	parser.add_argument('--list_files', action='store_true', help='Lista todos los ficheros pertenecientes al usuario')
 
-	parser.add_argument('--download_id', nargs=1, help='Recupera un fichero del servidor')
+	parser.add_argument('--download', nargs=1, help='Recupera un fichero del servidor', metavar=('id_fichero'))
 
-	parser.add_argument('--delete_file', nargs=1, help='Elimina un fichero del servidor')
+	parser.add_argument('--delete_file', nargs=1, help='Elimina un fichero del servidor', metavar=('id_fichero'))
 
 	# Cifrado y firma de ficheros local
-	parser.add_argument('--encrypt', nargs=1, help='Cifra un fichero para otro usuario indicado con --dest_id. Src_id necesario')
+	parser.add_argument('--encrypt', nargs=1, help='Cifra un fichero para otro usuario indicado con --dest_id. Src_id necesario', metavar=('fichero'))
 
-	parser.add_argument('--sign', nargs=1, help='Firma un fichero')
+	parser.add_argument('--sign', nargs=1, help='Firma un fichero', metavar=('fichero'))
 
-	parser.add_argument('--enc_sign', nargs=1, help='Encripta y firma un fichero')
+	parser.add_argument('--enc_sign', nargs=1, help='Encripta y firma un fichero', metavar=('fichero'))
 
 	args = parser.parse_args()
 
 	#Control para ver que solo se ha pasado el numero de comandos necesarios
 	count = 0
 	for item in vars(args).values():
-		if(item != None):
+		if(item != None and item!=False):
 			count += 1
+
+	#TODO: control de errores in here
+	d = read_dictionary()	
+	token = d['token']
 
 	if args.create_id:
 		if count != 1:
-			print "No es posible utilizar --create_id al mismo tiempo que otros parametros"
-			return
+			print  "ERROR: No es posible utilizar --create_id al mismo tiempo que otros parametros"
+			
 		else:
-			pass
-			#llamada a funcion
+			users.registro(nombre= args.create_id[0], email= args.create_id[1], alias= args.create_id[2], token= token)
 	elif args.search_id:
 		if count != 1:
-			print "No es posible utilizar --search_id al mismo tiempo que otros parametros"
-			return
+			print "ERROR:  No es posible utilizar --search_id al mismo tiempo que otros parametros"
+			
 		else:
-			pass
-			#llamada a funcion
+			users.buscar_identidad(data_search= args.search_id[0], token= token)
 	elif args.delete_id:
 		if count != 1:
-			print "No es posible utilizar --delete_id al mismo tiempo que otros parametros"
-			return
+			print " ERROR: No es posible utilizar --delete_id al mismo tiempo que otros parametros"
+			
 		else:
-			pass
-			#llamada a funcion
+			users.borrar_identidad(userID= args.delete_id[0], token= token)
 	elif args.upload:
-		if count != 3 or args.source_id == None or args.dest_id == None:
-			print "Para ejecutar --upload es necesario indicar --source_id y --dest_id."
-			print "No es posible utilizar --upload al mismo tiempo que otros parametros distintos a los anteriores"
-			return
+		if count != 2  or args.dest_id == None:
+			print "Para ejecutar --upload es necesario indicar --dest_id. \n No es posible utilizar --upload al mismo tiempo que otros parametros distintos a los anteriores"
+			
 		else:
-			pass
-			#llamada a funcion
+			files.upload(cadena= args.upload[0], token= token)
+
 	elif args.dest_id:
 		if args.upload == None and args.encrypt == None:
 			print "No tiene sentido utilizar --dest_id sin --upload o --encrypt"
-			return
+			
 	elif args.source_id:
 		if args.upload == None:
 			print "No tiene sentido utilizar --source_id sin --upload"
-			return
+			
 	elif args.list_files:
 		if count != 1:
 			print "No es posible utilizar --list_files al mismo tiempo que otros parametros"
-			return 
+			 
 		else:
-			pass 
-			#llamada a funcion
+			print  "Subiendo fichero con nombre: "+  args.upload[0]
+			files.list_files(cadena= args.create_id[0], token= token)
+
 	elif args.download:
 		if count != 1:
-			print "No es posible utilizar --download al mismo tiempo que otros parametros"
-			return
+			return "No es posible utilizar --download al mismo tiempo que otros parametros"
+			
 		else:
 			pass
 			#llamada a funcion
 	elif args.delete_file:
 		if count != 1:
-			print "No es posible utilizar --delete_file al mismo tiempo que otros parametros"
-			return
+			return "No es posible utilizar --delete_file al mismo tiempo que otros parametros"
+			
 		else:
 			pass
 			#llamada a funcion
 	elif args.encrypt:
 		if count != 1:
-			print "No es posible utilizar --encrypt al mismo tiempo que otros parametros"
-			return
+			return "No es posible utilizar --encrypt al mismo tiempo que otros parametros"
+			
 		else:
 			pass
 			#llamada a funcion
 	elif args.sign:
 		if count != 1:
-			print "No es posible utilizar --sign al mismo tiempo que otros parametros"
-			return
+			return "No es posible utilizar --sign al mismo tiempo que otros parametros"
+			
 		else:
 			pass
 			#llamada a funcion
 	elif args.enc_sign:
 		if count != 1:
-			print "No es posible utilizar --enc_sign al mismo tiempo que otros parametros"
-			return
+			return "No es posible utilizar --enc_sign al mismo tiempo que otros parametros"	
 		else:
 			pass
 			#llamada a funcion
+	else: 
+		return "ERROR: Utilice los argumentos correctamente. Para saber mas, utilice el comando:\n\n\tpython securebox_client.py --help\n"
+
+if __name__ == '__main__':
+   main()
--- securebox_users.py
+++ securebox_users.py
@@ -3,34 +3,43 @@ import requests
 
 # almacena un usuario, genera las claves y guarda su clave publica en el servidor
 # y la privada de forma local.
-def registro(nombre, email, token):
+def registro(nombre, email, alias, token):
 
 	#Generacion de claves para RSA
-	privateKey = RSA.generate(2048)
+	key = RSA.generate(2048)
 	#Se usa PEM porque lo dice el enunciado.
-	publicKey = privateKey.publicKey().exportKey('PEM')
+	privateKey = key.exportKey('PEM')
+	publicKey = key.publickey().exportKey('PEM')
 
+	print "Registrando usuario con:\n\t*Nombre: {}\n\t*Email: {}\n\t*Alias: {}".format(nombre, email, alias)
+	print "..."
 	#Escritura de la peticion de registro
 	url = 'https://vega.ii.uam.es:8080/api/users/register'
 	headers = {'Authorization': "Bearer " + token}
-	args = {'nombre': nombre, 'email': email, 'publicKey': publicKey}
+	args = {'nombre': nombre, 'email': email,'alias': alias, 'publicKey': publicKey}
 	
 	# Envio de solicitud, se almacena respuesta en r
 	r = requests.post(url, headers=headers, json=args)
 
 	#Guardamos la clave privada en un fichero solo si la respuesta tiene un codigo 200
 	if r.status_code == 200 :
-		with open("noEsLaClave.dat", "wb") as key_file:
+		with open("clave_privada.dat", "wb") as key_file:
 			key_file.write(privateKey)
 		key_file.closed
+		with open("clave_publica.dat", "wb") as pukey_file:
+			pukey_file.write(publicKey)
+		pukey_file.closed
 		with open("register.dat", "w") as reg_file:
-			reg_file.write(r.json()[nombre])
-			reg_file.write(r.json()[ts])
+			reg_file.write(r.text)
 		reg_file.closed
-		return OK
-	else :
-		return ERROR
 
+		print "OK\nEstos son sus credenciales:"
+		print "\t*Nombre: {}".format(r.json()['nombre'])
+		print "\t*ts: {}".format(r.json()['ts'])
+	else :
+		print "ERROR!!!\n No se ha podido completar el registro satisfactoriamente"
+
+	return
 
 #casi equivalente a buscar clave publica
 #userID: nombre de usuario o email
@@ -44,9 +53,10 @@ def buscar_clave_publica(userID, token):
 	r = requests.post(url, headers=headers, json=args)
 
 	if r.status_code == 200:
-		return r.json()[publicKey]
+		return r.json()['publicKey']
 	else:
-		return ERROR
+		print "ERROR. No se ha encontrado la clabe publica del usuario {}".format(userID)
+		return None
 
 #casi equivalente a buscar clave publica
 #return, en formato json, los siguientes campos:
@@ -58,6 +68,8 @@ def buscar_clave_publica(userID, token):
 #TODO: confirmar que json devuelve un diccionario(seria muy facil mapear en la respuesta eso)
 def buscar_identidad(data_search, token):
 
+	print "Buscando usuario {} en el servidor...".format(data_search)
+
 	#Escritura de la peticion de clave publica
 	url = 'https://vega.ii.uam.es:8080/api/users/search'
 	headers = {'Authorization': "Bearer " + token}
@@ -66,12 +78,25 @@ def buscar_identidad(data_search, token):
 	r = requests.post(url, headers=headers, json=args)
 
 	if r.status_code == 200:
-		return r.json()
+		print "OK"
+		d =  r.json()
+		#dkeys = d.keys()
+		print "{} usuarios encontrados".format(len(d))
+		count = 0
+		for item in d:
+			print "[{}] {}, {}, ID: {}".format(count+1, d[count]['nombre'], d[count]['email'], d[count]['userID'])
+			count += 1
 	else:
-		return ERROR
+		#return ERROR
+		print "No se han encontrado usuarios que concuerden con su busqueda"
+
+	return
 
 #borra una identidad syss esta ha sido creada por el propio usuario
-def delete_identity(userID):
+def borrar_identidad(userID, token):
+
+	print "Borrando usuario {} del servidor...".format(userID)
+
 	url = 'https://vega.ii.uam.es:8080/api/users/delete'
 	headers = {'Authorization': "Bearer " + token}
 	args = {'userID': userID}
@@ -79,7 +104,9 @@ def delete_identity(userID):
 	r = requests.post(url, headers=headers, json=args)
 
 	if r.status_code == 200:
-		return r.json()[userID]
+		print "OK"
+		print "El usuario con ID {} ha sido eliminado satisfactoriamente".format(r.json()['userID'])
 	else:
-		return ERROR
+		print "ERROR. No se ha podido borrar el usuario"
 
+	return	
